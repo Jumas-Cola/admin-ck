df49642b94e7827eee9d39d5c39c071b5263365d
Jumas Cola <kbbyfl91@gmail.com>
2021-11-01 13:57:48 +0300

    upd admin-ck uploader

--- src/CKUploader.php
+++ src/CKUploader.php
@@ -27,9 +27,9 @@ function selectFileWithCKFinder( elementId ) {
                 const path = parsed_url.pathname;
                 const splitted_path = path.split('/');
                 const l = splitted_path.length;
-                url = '/' + splitted_path[l-2] + '/' + splitted_path[l-1];
-                document.getElementById( 'ckfinder-input-' + elementId ).value = url;
-                document.getElementById( 'ckfinder-image-' + elementId).src = path;
+                relUrl = '/' + splitted_path[l-2] + '/' + splitted_path[l-1];
+                document.getElementById( 'ckfinder-input-' + elementId ).value = relUrl;
+                document.getElementById( 'ckfinder-image-' + elementId).src = url;
             } );
             finder.on( 'file:choose:resizedImage', function( evt ) {
                 document.getElementById( 'ckfinder-input-' + elementId ).value = evt.data.resizedUrl;
